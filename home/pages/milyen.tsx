3cbb405d7ba3f94624327320048d035358f41a6b
Akos Toth-Mate <akos@tothmate.com>
2021-11-08 10:14:19 +0100

    green is the new yellow

--- home/pages/milyen.tsx
+++ home/pages/milyen.tsx
@@ -16,14 +16,14 @@ const content = [
       "releváns",
     ],
   },
-  { title: "Teret ad a vitáknak és véleményeknek", list: [] },
+  { title: "Teret ad a vitáknak és véleményeknek", list: ["asfsdfas"] },
   { title: "Ellenőrzi a társadalomban hatalommal bírókat", list: [] },
   { title: "Feltárja az igazságot", list: [] },
   { title: "Láthatóvá tesz és felhangosít ügyeket", list: [] },
   { title: "Oktatja és neveli az állampolgárokat", list: [] },
 ];
 
-const gradient = tinygradient("#5ac7fa", "#ffcc02");
+const gradient = tinygradient("#5ac8fa", "#32d74b");
 
 const Page = () => (
   <Layout darkLogo="secondary">
