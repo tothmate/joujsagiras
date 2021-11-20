bd0ae0ddbdd89aeea630c2fa4ffe15b1bf08645f
Akos Toth-Mate <akos@tothmate.com>
2021-11-20 13:28:44 +0100

    hopp swaps Oswald font too

--- hopp/pages/_document.tsx
+++ hopp/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald&display=optional" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" />
         </Head>
         <body>
           <Main />
