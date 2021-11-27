f816fe1c9f2fe0244c6e8ff7b673d41ccd32aee5
bujatt <bujatt@gmail.com>
2021-11-27 16:52:44 +0100

    Added 5 more 'bad' attributes

--- hopp/src/models.ts
+++ hopp/src/models.ts
@@ -42,22 +42,69 @@ export const reasons: Reason[] = [
     text: 'nem tényszerű',
     details: [
       'A jó újságírás tényszerűen tájékoztat, ezért ellenőrzi a tényszerűségét annak ami ír.',
-      'A nem tényszerű újságírás félrevezeti olvasóját – akár szándékosan vagy hanyagságból teszi.'
+      'A nem valósághű újságírás félrevezeti olvasóját – akár szándékosan vagy hanyagságból teszi.'
+    ]
+  },
+  {
+    slug: 'nem-ellenorizheto',
+    text: 'nem ellenőrizhető',
+    details: [
+      'A jó újságírás ellenőrizhetően tájékoztat, ezért visszakövethetővé teszi a hivatkozott tényeket.',
+      'A nem vagy nehezen ellenőrizhető újságírás könnyen elferdíti a valóságot és félrevezeti olvasóját – akár szándékosan vagy hanyagságból teszi.'
+    ]
+  },
+  {
+    slug: 'nem-kiegyensulyozott',
+    text: 'nem kiegyensúlyozott',
+    details: [
+      'A jó újságírás kiegyensúlyozottan tájékoztat, ezért megkérdez és meghallgat minden releváns felet.',
+      'A nem kiegyensúlyozott újságírás részrehajló és félrevezeti az olvasóját – akár szándékosan vagy hanyagságból teszi.'
+    ]
+  },
+  {
+    slug: 'nem-kiegyensulyozott',
+    text: 'nem kiegyensúlyozott',
+    details: [
+      'A jó újságírás kiegyensúlyozottan tájékoztat, ezért megkérdez és meghallgat minden releváns felet.',
+      'A nem kiegyensúlyozott újságírás részrehajló és félrevezeti az olvasóját – akár szándékosan vagy hanyagságból teszi.'
     ]
   },
   {
     slug: 'nem-hiteles',
     text: 'nem hiteles',
     details: [
-      'A jó újságírás hitelesen tájékoztat, ezért visszakövethetővé és ezzel ellenőrizhetővé teszi az írásban hivatkozott tényeket.',
-      'A nem hiteles újságírás – mivel nehezebben ellenőrizhető – könnyebben elferdítheti a valóságot.'
+      'A jó újságírás hitelesen tájékoztat, ezért a szerző a saját nevével vállalja amit ír.',
+      'Az az újságíró vagy szerkesztőség, aki nem vállalja fel névvel amit ír, könnyen enged meg magának szakmaiatlan, félrevezető vagy rossz munkát – akár szándékosan vagy hanyagságból teszi.'
     ]
   },
   {
+    slug: 'nem-ertelmezheto',
+    text: 'nem értelmezhető',
+    details: [
+      'A jó újságírás könnyen értelmezhető módon tájékoztat, ezért kontextusba helyezi a mondanivalóját és ezzel segít értelmezni azt.',
+      'Az olyan újságírás, amely nem vagy hiányosan mutatja be egy ügy körülményeit, vagy kiragadja kontextusából, félrevezeti az olvasóját – akár szándékosan vagy hanyagságból teszi.'
+    ]
+  },
+  {
+    slug: 'nem-oszinte',
+    text: 'nem őszinte',
+    details: [
+      'A jó újságírás őszintén tájékoztat, ezért az elkövetett hibákat egyértelműen és őszintén beismeri, ha változik egy cikk, azt jelöli, ha újabb információ érkezik be, azt közzéteszi.',
+      'A nem őszinte újságírás elhallgat tévedéseket, hibákat vagy a cikkhez kapcsolódó beérkező információkat, ezzel félrevezeti az olvasóját – akár szándékosan vagy hanyagságból teszi.'
+    ]
+  },
+  {
+    slug: 'nem-relevans',
+    text: 'nem releváns',
+    details: [
+      'A jó újságírás minden eseményt időszerűen, a jelentőségének megfelelő súllyal és terjedelemben dolgoz fel.',
+      'A rossz újságírás nem létező ügyeket kreál, piszlicsáré ügyeket hangosít fel vagy éppen elhallgat fontos ügyeket.'
+    ]
+  },  {
     slug: 'nem-pontos',
     text: 'nem pontos',
     details: [
-      'A jó újságírás pontosan tájékoztat, ezért adatokkal támasztja alá az állításait.',
+      'A jó újságírás pontosan tájékoztat, ezért adatokkal, táblázatokkal, ábrákkal támasztja alá az állításait.',
       'A pontatlan újságírás félrevezeti olvasóját – akár szándékosan vagy hanyagságból teszi.'
     ]
   }
