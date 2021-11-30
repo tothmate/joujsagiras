9fd5f4589a1df566a3c5c324dccce35d2cad89a4
Akos Toth-Mate <akos@tothmate.com>
2021-11-30 22:00:47 +0100

    debug like there's no tomorrow

--- hopp/pages/api/generate-sticker.ts
+++ hopp/pages/api/generate-sticker.ts
@@ -17,7 +17,71 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   });
 
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle0" });
+
+  page.on("AAA close", (msg) => {
+    console.log("close", msg);
+  });
+  page.on("AAA console", (msg) => {
+    console.log("console", msg);
+  });
+  page.on("AAA dialog", (msg) => {
+    console.log("dialog", msg);
+  });
+  page.on("AAA domcontentloaded", (msg) => {
+    console.log("domcontentloaded", msg);
+  });
+  page.on("AAA error", (msg) => {
+    console.log("error", msg);
+  });
+  page.on("AAA frameattached", (msg) => {
+    console.log("frameattached", msg);
+  });
+  page.on("AAA framedetached", (msg) => {
+    console.log("framedetached", msg);
+  });
+  page.on("AAA framenavigated", (msg) => {
+    console.log("framenavigated", msg);
+  });
+  page.on("AAA load", (msg) => {
+    console.log("load", msg);
+  });
+  page.on("AAA metrics", (msg) => {
+    console.log("metrics", msg);
+  });
+  page.on("AAA pageerror", (msg) => {
+    console.log("pageerror", msg);
+  });
+  page.on("AAA popup", (msg) => {
+    console.log("popup", msg);
+  });
+  page.on("AAA request", (msg) => {
+    console.log("request", msg);
+  });
+  page.on("AAA requestfailed", (msg) => {
+    console.log("requestfailed", msg);
+  });
+  page.on("AAA requestfinished", (msg) => {
+    console.log("requestfinished", msg);
+  });
+  page.on("AAA response", (msg) => {
+    console.log("response", msg);
+  });
+  page.on("AAA workercreated", (msg) => {
+    console.log("workercreated", msg);
+  });
+  page.on("AAA workerdestroyed", (msg) => {
+    console.log("workerdestroyed", msg);
+  });
+
+  console.log("goto", url);
+
+  page.setDefaultTimeout(5000);
+
+  try {
+    await page.goto(url, { waitUntil: "networkidle0" });
+  } catch (e) {
+    console.log("error", e);
+  }
 
   const height = await page.evaluate(() => {
     return document.body.clientHeight;
