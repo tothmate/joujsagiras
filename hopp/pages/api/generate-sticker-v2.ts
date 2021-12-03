5b19532c5adc349685d52719aafa08dd0aef8e7a
Akos Toth-Mate <akos@tothmate.com>
2021-12-03 13:25:11 +0100

    revert debug

--- hopp/pages/api/generate-sticker-v2.ts
+++ hopp/pages/api/generate-sticker-v2.ts
@@ -9,7 +9,7 @@ if (
 }
 
 import { NextApiRequest, NextApiResponse } from "next";
-// import { createCanvas } from "canvas";
+import { createCanvas } from "canvas";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!req.headers.host || !req.url || !req.query.reasonSlug || !req.query.stickerId) {
@@ -17,27 +17,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  res.status(200).json({ path: process.env.LD_LIBRARY_PATH });
+  const canvas = createCanvas(200, 200);
+  const ctx = canvas.getContext("2d");
 
-  //   const canvas = createCanvas(200, 200);
-  //   const ctx = canvas.getContext("2d");
+  ctx.font = "30px Impact";
+  ctx.rotate(0.1);
+  ctx.fillText("Awesome!", 50, 100);
 
-  //   ctx.font = "30px Impact";
-  //   ctx.rotate(0.1);
-  //   ctx.fillText("Awesome!", 50, 100);
+  var text = ctx.measureText("Awesome!");
+  ctx.strokeStyle = "rgba(0,0,0,0.5)";
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + text.width, 102);
+  ctx.stroke();
 
-  //   var text = ctx.measureText("Awesome!");
-  //   ctx.strokeStyle = "rgba(0,0,0,0.5)";
-  //   ctx.beginPath();
-  //   ctx.lineTo(50, 102);
-  //   ctx.lineTo(50 + text.width, 102);
-  //   ctx.stroke();
+  const screenshot = canvas.createPNGStream();
 
-  //   const screenshot = canvas.createPNGStream();
-
-  //   res.setHeader("Content-Type", "image/png");
-  //   if (!process.env.IS_LOCAL) {
-  //     res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate=2678400");
-  //   }
-  //   res.status(200).send(screenshot);
+  res.setHeader("Content-Type", "image/png");
+  if (!process.env.IS_LOCAL) {
+    res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate=2678400");
+  }
+  res.status(200).send(screenshot);
 }
