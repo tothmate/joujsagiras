a0cebc4b69976dc79727988b7bb5729172a78882
Akos Toth-Mate <akos@tothmate.com>
2021-12-04 23:17:53 +0100

    debug

--- hopp/pages/debug.tsx
+++ hopp/pages/debug.tsx
@@ -12,42 +12,11 @@ export async function getServerSideProps() {
 }
 
 export default function Page(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [copiedLink, setCopiedLink] = useState(false);
-  const link = getUrlForSticker(props.sticker, GeneratorMode.Share);
-
   return (
     <>
-      <Typography variant="h1" gutterBottom>
-        HOPP! Ez {props.sticker.reason.text}.
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        {props.sticker.explanation || "Hogy miért baj ez?"}
-      </Typography>
-      <Typography variant="h1" gutterBottom>
-        Ez nem jó újságírás.
-      </Typography>
-      {props.sticker.reason.details.map((detail) => (
-        <Typography variant="body1" gutterBottom key={detail}>
-          {detail}
-        </Typography>
-      ))}
-      <CopyToClipboard
-        text={link}
-        onCopy={() => {
-          setCopiedLink(true);
-          setTimeout(() => setCopiedLink(false), 2000);
-        }}
-      >
-        <Button
-          startIcon={copiedLink ? <Done /> : <ContentCopy />}
-          variant="contained"
-          color={copiedLink ? "success" : "secondary"}
-          size="large"
-          fullWidth
-        >
-          Link másolása
-        </Button>
-      </CopyToClipboard>
+      <Button startIcon={<Done />} variant="contained" color="secondary" size="large" fullWidth>
+        Link másolása
+      </Button>
       <Button
         startIcon={<Facebook />}
         variant="contained"
@@ -57,7 +26,7 @@ export default function Page(props: InferGetServerSidePropsType<typeof getServer
         fullWidth
         onClick={() => {
           window.open(
-            `https://www.facebook.com/sharer/sharer.php?u=${link}`,
+            `https://www.facebook.com/sharer/sharer.php?u=s`,
             "",
             "width=550, height=400, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0"
           );
