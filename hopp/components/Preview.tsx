a2555ad0d6b4e27e25746e1cc92c748755bb2778
Akos Toth-Mate <akos@tothmate.com>
2021-12-04 14:23:56 +0100

    debug

--- hopp/components/Preview.tsx
+++ hopp/components/Preview.tsx
@@ -22,43 +22,42 @@ export default function Preview(props: { sticker: Sticker; useCanvas: boolean })
   });
 
   return (
-    <>xx</>
-    // <Card>
-    //   {props.useCanvas && (
-    //     <CardMedia sx={{ lineHeight: 0 }}>
-    //       <canvas ref={canvasRef} style={{ width: "100%", aspectRatio: "1.91" }} />
-    //     </CardMedia>
-    //   )}
-    //   {!props.useCanvas && (
-    //     <CardMedia
-    //       image={getUrlForSticker(props.sticker, GeneratorMode.Png)}
-    //       title={props.sticker.source.title}
-    //       style={{ width: "100%", aspectRatio: "1.91" }}
-    //     />
-    //   )}
-    //   <CardContent
-    //     sx={{
-    //       padding: "0 12px",
-    //       "&:last-child": {
-    //         paddingBottom: "6px",
-    //       },
-    //       backgroundColor: (theme) => theme.palette.grey[100],
-    //     }}
-    //   >
-    //     <Link
-    //       href={props.sticker.source.url}
-    //       underline="hover"
-    //       sx={(theme) => ({ textTransform: "uppercase", fontSize: "0.7em", color: theme.palette.text.secondary })}
-    //       variant="body2"
-    //       noWrap
-    //       target="_blank"
-    //     >
-    //       {getSourceHostname(props.sticker)}
-    //     </Link>
-    //     <Typography variant="subtitle1" component="h3" fontWeight="bold" lineHeight="1.5em">
-    //       {props.sticker.source.title}
-    //     </Typography>
-    //   </CardContent>
-    // </Card>
+    <Card>
+      {props.useCanvas && (
+        <CardMedia sx={{ lineHeight: 0 }}>
+          <canvas ref={canvasRef} style={{ width: "100%", aspectRatio: "1.91" }} />
+        </CardMedia>
+      )}
+      {!props.useCanvas && (
+        <CardMedia
+          image={getUrlForSticker(props.sticker, GeneratorMode.Png)}
+          title={props.sticker.source.title}
+          style={{ width: "100%", aspectRatio: "1.91" }}
+        />
+      )}
+      <CardContent
+        sx={{
+          padding: "0 12px",
+          "&:last-child": {
+            paddingBottom: "6px",
+          },
+          backgroundColor: (theme) => theme.palette.grey[100],
+        }}
+      >
+        <Link
+          href={props.sticker.source.url}
+          underline="hover"
+          sx={(theme) => ({ textTransform: "uppercase", fontSize: "0.7em", color: theme.palette.text.secondary })}
+          variant="body2"
+          noWrap
+          target="_blank"
+        >
+          {getSourceHostname(props.sticker)}
+        </Link>
+        <Typography variant="subtitle1" component="h3" fontWeight="bold" lineHeight="1.5em">
+          {props.sticker.source.title}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
