aac1e802a4a9ebc31fcb8ae7210fe0b1a05ad69e
Akos Toth-Mate <akos@tothmate.com>
2021-12-04 16:21:06 +0100

    degraded run on vercel

--- hopp/pages/api/generate-sticker.ts
+++ hopp/pages/api/generate-sticker.ts
@@ -3,15 +3,12 @@ import { createCanvas, registerFont } from "canvas";
 import store from "../../src/SupabaseStore";
 import { StickerStoreErrorType } from "../../src/models";
 import { drawPreview } from "../../src/canvas";
-import { saveOswaldIfNeeded } from "../../src/font";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const result = await store.load(req.query.stickerId as string);
   result.match(
     async (sticker) => {
-      const oswaldFilename = "Oswald-VariableFont_wght.ttf";
-      saveOswaldIfNeeded(oswaldFilename);
-      registerFont(oswaldFilename, { family: "Oswald" });
+      // registerFont("Oswald-VariableFont_wght.ttf", { family: "Oswald" });
       const canvas = createCanvas(1200, 628);
       await drawPreview(canvas.getContext("2d"), sticker.source.image, sticker.reason.text);
       const screenshot = canvas.createPNGStream();
