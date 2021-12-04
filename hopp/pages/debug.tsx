eeec753f61e70d7c84e428dace6f797186d495b9
Akos Toth-Mate <akos@tothmate.com>
2021-12-04 23:26:14 +0100

    is it you, window

--- hopp/pages/debug.tsx
+++ hopp/pages/debug.tsx
@@ -1,11 +1,8 @@
 import { InferGetServerSidePropsType } from "next";
 import { emptySticker } from "../src/models";
-import React, { useState } from "react";
-import { Button, Typography } from "@mui/material";
-import { Sticker, GeneratorMode } from "../src/models";
-import { getUrlForSticker } from "../src/helpers";
-import CopyToClipboard from "react-copy-to-clipboard";
-import { ContentCopy, Done, Facebook } from "@mui/icons-material";
+import React from "react";
+import { Button } from "@mui/material";
+import { Done, Facebook } from "@mui/icons-material";
 
 export async function getServerSideProps() {
   return { props: {} };
@@ -17,21 +14,7 @@ export default function Page(props: InferGetServerSidePropsType<typeof getServer
       <Button startIcon={<Done />} variant="contained" color="secondary" size="large" fullWidth>
         Link másolása
       </Button>
-      <Button
-        startIcon={<Facebook />}
-        variant="contained"
-        sx={{ mt: 2 }}
-        color="primary"
-        size="large"
-        fullWidth
-        onClick={() => {
-          window.open(
-            `https://www.facebook.com/sharer/sharer.php?u=s`,
-            "",
-            "width=550, height=400, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0"
-          );
-        }}
-      >
+      <Button startIcon={<Facebook />} variant="contained" sx={{ mt: 2 }} color="primary" size="large" fullWidth>
         Megosztás
       </Button>
     </>
