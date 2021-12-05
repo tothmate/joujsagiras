4dd3e7263dc636ce5ada43dec875f015c0df04cd
Akos Toth-Mate <akos@tothmate.com>
2021-12-05 18:22:39 +0100

    debug

--- hopp/pages/api/debug.ts
+++ hopp/pages/api/debug.ts
@@ -7,20 +7,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   saveOswaldIfNeeded(fontFilepath);
   registerFont(fontFilepath, { family: "Oswald" });
   const canvas = createCanvas(1200, 628);
-
-  const imageUrl = "https://cdn.nwmgroups.hu/s/img/i/2006/20200629chinas-state-media-warns-soros.jpg";
-  const text = "HOPP! EZ NEM ELLENŐRIZHETŐ.";
   const ctx = canvas.getContext("2d");
 
-  const heightRatio = 0.25;
-  const textSizeRatio = 0.55;
-
   ctx.fillStyle = "#000000";
-  ctx.font = `${canvas.height * heightRatio * textSizeRatio}px Oswald, sans-serif`;
-  ctx.fillText(text, canvas.width * 0.02, canvas.height * heightRatio * (0.5 + 0.5 * textSizeRatio), canvas.width);
+  ctx.font = "86px Oswald, sans-serif";
+  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 121, 1200);
 
   const screenshot = canvas.createPNGStream();
-
   res.setHeader("Content-Type", "image/png");
   res.status(200).send(screenshot);
 }
