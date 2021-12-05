8a9683c2ea0aeedc00cbd66e70bcc6ec8f48bb2f
Akos Toth-Mate <akos@tothmate.com>
2021-12-05 19:37:07 +0100

    debug

--- hopp/pages/api/debug.ts
+++ hopp/pages/api/debug.ts
@@ -14,7 +14,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   ctx.fillStyle = "#000000";
   ctx.scale(6, 6);
-  ctx.fillText("HOPP", 0, 50, 2000);
+  ctx.fillText("HOPP", 0, 50, 1000);
+  ctx.fillText("HOPP", 10, 50, 5000);
+  ctx.fillText("HOPP", 20, 50);
 
   const screenshot = canvas.createPNGStream();
   res.setHeader("Content-Type", "image/png");
