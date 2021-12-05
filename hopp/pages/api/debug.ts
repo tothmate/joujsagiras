69a52199b5161a8be512fb7529ee933a2c484cb7
Akos Toth-Mate <akos@tothmate.com>
2021-12-05 18:41:40 +0100

    debug

--- hopp/pages/api/debug.ts
+++ hopp/pages/api/debug.ts
@@ -10,43 +10,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const ctx = canvas.getContext("2d");
 
   ctx.fillStyle = "#000000";
-  ctx.font = "86px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 100, 500);
-
-  ctx.font = "46px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 140, 500);
-
-  ctx.font = "26px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 160, 500);
-
-  ctx.font = "16px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 170, 500);
-
-  ctx.fillStyle = "#ff0000";
-  ctx.font = "86px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 30, 100, 5000);
-
-  ctx.font = "46px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 30, 140, 5000);
-
-  ctx.font = "26px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 30, 160, 5000);
-
-  ctx.font = "16px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 30, 170, 5000);
-
-  ctx.fillStyle = "#00ff00";
-  ctx.font = "86px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 60, 100);
-
-  ctx.font = "46px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 60, 140);
-
-  ctx.font = "26px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 60, 160);
-
-  ctx.font = "16px sans-serif";
-  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 60, 170);
+  ctx.font = "12px Oswald";
+  ctx.save();
+  ctx.translate(0, 86);
+  ctx.scale(86.0 / 12.0, 86.0 / 12.0);
+  ctx.fillText("HOPP! EZ NEM ELLENŐRIZHETŐ.", 0, 0);
+  ctx.restore();
 
   const screenshot = canvas.createPNGStream();
   res.setHeader("Content-Type", "image/png");
