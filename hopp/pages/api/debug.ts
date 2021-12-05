a23f8a84dfc0ffb11bb0e41f86912623e9da920e
Akos Toth-Mate <akos@tothmate.com>
2021-12-05 19:01:25 +0100

    debug

--- hopp/pages/api/debug.ts
+++ hopp/pages/api/debug.ts
@@ -1,23 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createCanvas, registerFont } from "canvas";
-import { saveOswaldIfNeeded } from "../../src/font";
+import { createCanvas } from "canvas";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const fontFilepath = "/tmp/Oswald-VariableFont_wght.ttf";
-  saveOswaldIfNeeded(fontFilepath);
-  registerFont(fontFilepath, { family: "Oswald" });
-  const canvas = createCanvas(1200, 628);
+  const canvas = createCanvas(800, 600);
   const ctx = canvas.getContext("2d");
 
   ctx.fillStyle = "#000000";
+  ctx.font = "24px serif";
+  ctx.scale(2, 2);
+  ctx.fillText("HOPP", 0, 50);
+
   ctx.font = "12px serif";
-  ctx.save();
-  ctx.translate(0, 86);
-  ctx.scale(86.0 / 12.0, 86.0 / 12.0);
-  ctx.fillText("HOPP", 0, 0, 50000);
-  ctx.restore();
-  ctx.fillText("HOPP", 0, 150);
-  ctx.fillText("HOPP", 0, 250, 50000);
+  ctx.scale(2, 2);
+  ctx.fillText("HOPP", 0, 100);
 
   const screenshot = canvas.createPNGStream();
   res.setHeader("Content-Type", "image/png");
