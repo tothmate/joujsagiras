838b7ed3b9c6eb56542356313d681f54d4520577
Akos Toth-Mate <akos@tothmate.com>
2021-12-06 21:14:21 +0100

    usecanvas in viewer until I can fix server side canvas

--- hopp/next.config.js
+++ hopp/next.config.js
@@ -1,14 +1,3 @@
-console.log("before", process.env.LD_LIBRARY_PATH);
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
-) {
-  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
-    process.env.LD_LIBRARY_PATH || ""
-  }`;
-}
-console.log("after", process.env.LD_LIBRARY_PATH);
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   basePath: "/hopp",
