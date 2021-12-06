849c919f0ce5e707df0977d7b2d903fdc6863ff8
Akos Toth-Mate <akos@tothmate.com>
2021-12-06 20:52:15 +0100

    playing with LD_LIBRARY_PATH again to load the right zlib

--- hopp/next.config.js
+++ hopp/next.config.js
@@ -1,22 +1,33 @@
+console.log("before", process.env.LD_LIBRARY_PATH);
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(`${process.env.PWD}/node_modules/canvas/build/Release:`)
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD}/node_modules/canvas/build/Release:${
+    process.env.LD_LIBRARY_PATH || ""
+  }`;
+}
+console.log("after", process.env.LD_LIBRARY_PATH);
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: '/hopp',
+  basePath: "/hopp",
   reactStrictMode: false,
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/create',
+        source: "/",
+        destination: "/create",
         permanent: true,
-      }
-    ]
+      },
+    ];
   },
   async rewrites() {
     return [
       {
-        source: '/:reasonSlug/:stickerId.png',
-        destination: '/api/generate-sticker/?stickerId=:stickerId',
+        source: "/:reasonSlug/:stickerId.png",
+        destination: "/api/generate-sticker/?stickerId=:stickerId",
       },
-    ]
-  }
-}
+    ];
+  },
+};
