14adb336c35f96d333f2ea36c465be6f371b0c97
Akos Toth-Mate <akos@tothmate.com>
2021-12-10 13:52:53 +0100

    latin extended preload

--- hopp/pages/_document.tsx
+++ hopp/pages/_document.tsx
@@ -7,7 +7,10 @@ export default class MyDocument extends Document {
       <Html lang="hu">
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Oswald&display=swap&subset=latin-extended"
+          />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         </Head>
         <body>
