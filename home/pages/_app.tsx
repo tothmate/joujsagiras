ad1572449d729df95d52434a07aa19d3c091ee75
Akos Toth-Mate <akos@tothmate.com>
2021-12-11 12:56:55 +0100

    umami analytics added

--- home/pages/_app.tsx
+++ home/pages/_app.tsx
@@ -6,6 +6,12 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Jó újságírás</title>
+        <script
+          async
+          defer
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+          src="https://analytics.joujsagiras.hu/umami.js"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
--- hopp/pages/_document.tsx
+++ hopp/pages/_document.tsx
@@ -9,6 +9,12 @@ export default class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <script
+            async
+            defer
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+            src="https://analytics.joujsagiras.hu/umami.js"
+          ></script>
         </Head>
         <body>
           <Main />
